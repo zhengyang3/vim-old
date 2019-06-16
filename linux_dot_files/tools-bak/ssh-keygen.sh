5c0273970eeac70dbedbaafe237994dbb57f605a
Fermin <Fermin@foxmail.com>
2019-06-16 01:36:24 -0700

    opt

--- linux_dot_files/tools-bak/ssh-keygen.sh
+++ linux_dot_files/tools-bak/ssh-keygen.sh
@@ -5,3 +5,11 @@ cd ~
 ssh-keygen -t rsa -C "zhengyang3@qq.com" 
 
 cat ~/.ssh/id_rsa.pub
+# xshell can identify long line, so you needn't to use xclip, 
+# you just use mouse to select the line, that's so nice! enjoy it!  
+
+
+# Note：
+    # if old/exist git repository use https , you should change .git/config url:
+    #  https://gitee.com/zhengyang3/vim.git   --->    git@gitee.com:zhengyang3/vim.git
+    
